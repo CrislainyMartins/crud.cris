0a59067d76e2fd45678046439b267179957fb665
CrislainyMartins <crislainy6339@gmail.com>
2024-02-29 14:57:43 -0300

    verificaçao 03

---
+++ cadastro.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário</title>
+
+</head>
+<body>
+    <div class="box">
+        <form action="">
+            <fieldset><!--cria um quadrado em forma de linha ao redor das informações-->
+            <legend><b>Formulário de Clientes</b></legend><!--b significa negrito-->
+            <br>
+            <div class="inputBox">
+                <input tipy="tex"
+            </div>
+
+            </fieldset>
+        </form>
+    </div>
+</body>
+</html>
